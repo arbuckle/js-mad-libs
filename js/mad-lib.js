7fef7c646d03c9038c977fdb3d17f663007beaeb
David Arbuckle <dintlu@gmail.com>
2012-06-12 08:26:54 -0400

    Created word scrambler.  The word scrambler breaks the mad lib down into a list of individual words and replaces each word with a completely random string.
    A better implementation might be to jumble each word in place, which is
    something to explore.

--- js/mad-lib.js
+++ js/mad-lib.js
@@ -12,9 +12,9 @@ var Mad = {
 
 	 Lib: function(html) {
 	 	this.original_article = html;
-	 	this.article = html;
-	 	this._populateWords(this.article);
-	 	console.log(this.words);
+		this.article = this._getRequiredWords(this.original_article);
+		console.log(this._scramble(this.article));
+		console.log(this.words);
 	 },
 
 	 _getRequiredWords: function(article) {
@@ -36,10 +36,47 @@ var Mad = {
 	 			this.words[word_type].push(null);
 	 		}
 
-	 		return this._populateWords(article);
+			return this._getRequiredWords(article);
 	 	} else {
 	 		return article;
 	 	}
+	 },
+
+	 _scramble: function(article) {
+		var i, j, k,
+			p_len,
+			word = '',
+			rand = 0,
+			key_space = 'abcdefghijklmnopqrstuvwxyz',
+			article = article.split('\n'),
+			a_len = article.length;
+
+		/* splitting article paragraphs into individual words */
+		for (i=0; i < a_len; i ++) {
+			article[i] = article[i].split(' ');
+		}
+
+		/* scrambling words and rejoining paragraphs */
+		for (i=0; i < a_len; i ++) {
+			p_len = article[i].length;
+
+			for (j=0; j < p_len; j ++) {
+				if (article[i][j].search(/<.*>/) > -1) {
+					//console.log(article[i][j])
+					//do nothing when the word is an HTML tag.
+				} else {
+					for (k in article[i][j]) {
+						rand = Math.floor(Math.random() * 26);
+						word += key_space[rand];
+					}
+					article[i][j] = word;
+					word = '';
+				}
+			}
+			article[i] = article[i].join(' ');
+		}
+		article = article.join(' ');
+		return article;
 	 }
 
 }
